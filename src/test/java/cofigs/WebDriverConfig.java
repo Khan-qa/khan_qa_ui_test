38326c4e3f15a625c9330ff0847198f0900e28ed
KKaiyrbekov <han_umag@mail.com>
2023-01-25 00:24:34 +0600

    last commit

--- src/test/java/cofigs/WebDriverConfig.java
+++ src/test/java/cofigs/WebDriverConfig.java
@@ -3,7 +3,7 @@ package cofigs;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:${env}.properties"
+        "classpath:remote.properties"
 })
 public interface WebDriverConfig extends Config {
 
